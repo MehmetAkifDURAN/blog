dd25ae7087a73343af25529b52db2f2e1ad5a547
Mehmet Akif Duran <mehmetakifduran2020@gmail.com>
2022-09-14 18:28:25 +0300

    Modellere meta class'lar eklendi. Migration yapıldı. Modeller için admin class'ları oluşturuldu.

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from .models import BlogPost, Code, Text
 # Register your models here.
+
+# class CourseAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'is_active', 'unit_price', 'slug',
+#                     'instructor', 'category_level_two')
+#     prepopulated_fields = {'slug': ('name',)}
+#     search_fields = ('name', 'description')
+#     list_filter = ('is_active', 'instructor', 'category_level_two')
+# admin.site.register(Course, CourseAdmin)
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'slug', 'blogger')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'description', 'blogger')
+    list_filter = ('is_active', 'blogger')
+
+
+class CodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'order_number', 'blog_post')
+    search_fields = ('blog_post',)
+    list_filter = ('blog_post',)
+
+
+class TextAdmin(admin.ModelAdmin):
+    list_display = ('text', 'order_number', 'blog_post')
+    search_fields = ('text', 'blog_post')
+    list_filter = ('blog_post',)
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Code, CodeAdmin)
+admin.site.register(Text, TextAdmin)
--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -14,6 +14,13 @@ class BlogPost(models.Model):
     blogger = models.ForeignKey(
         to=User, on_delete=models.CASCADE, verbose_name='Yazar', related_name='blog_posts')
 
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Bloglar'
+
+    def __str__(self):
+        return f'{self.title} - {self.blogger}'
+
 
 class Code(models.Model):
     code = models.TextField(verbose_name='Kod')
@@ -21,9 +28,23 @@ class Code(models.Model):
     blog_post = models.ForeignKey(
         to=BlogPost, on_delete=models.CASCADE, verbose_name='Blog Yazısı', related_name='codes')
 
+    class Meta:
+        verbose_name = 'Kod'
+        verbose_name_plural = 'Kodlar'
+
+    def __str__(self):
+        return f'{self.code[:20]}... - {self.blog_post}'
+
 
 class Text(models.Model):
     text = models.TextField(verbose_name='Metin')
     order_number = models.IntegerField(verbose_name='Sıra No')
     blog_post = models.ForeignKey(
         to=BlogPost, on_delete=models.CASCADE, verbose_name='Blog Yazısı', related_name='texts')
+
+    class Meta:
+        verbose_name = 'Metin'
+        verbose_name_plural = 'Metinler'
+
+    def __str__(self):
+        return f'{self.text[:20]}... - {self.blog_post}'
